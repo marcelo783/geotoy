230d237b345db31ea36c224c10885b3ac3a8a600
marcelo783 <marcelo.oliveira7931@gmail.com>
2025-10-07 23:08:47 -0300

    deploy backend nestjs

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+await app.listen(process.env.PORT || 3000)
   console.log('🚀 Server running at http://localhost:3000');
 }
 bootstrap();
