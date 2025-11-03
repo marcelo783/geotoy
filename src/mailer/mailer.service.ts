b03bb110ec4d517fb738e2b8eeca257bc99f7371
marcelo783 <marcelo.oliveira7931@gmail.com>
2025-11-02 21:42:36 -0300

    fix: trocando mailgun.js por mailgun-js

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,27 +1,23 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-import Mailgun from 'mailgun.js';
-import formData from 'form-data'; // 👈 Import correto
+import mailgun from 'mailgun-js';
 
 @Injectable()
 export class MailerService {
-  private mg: any;
+  private mg: mailgun.Mailgun;
   private domain: string;
   private from: string;
   private logger = new Logger(MailerService.name);
 
   constructor() {
-    // 👇 Aqui está o segredo: garantir o uso de "form-data" para Node
-    const mailgun = new Mailgun(formData);
-
-    this.mg = mailgun.client({
-      username: 'api',
-      key: process.env.MAILGUN_API_KEY!,
-    });
-
     this.domain = process.env.MAILGUN_DOMAIN!;
     this.from = process.env.MAILGUN_FROM!;
+
+    this.mg = mailgun({
+      apiKey: process.env.MAILGUN_API_KEY!,
+      domain: this.domain,
+    });
   }
 
   async sendEmailWithImages(
@@ -31,14 +27,6 @@ export class MailerService {
     attachments?: { filename: string; path: string; cid?: string }[],
   ) {
     try {
-      // Converte os anexos em streams
-      const formattedAttachments =
-        attachments?.map((a) => ({
-          filename: a.filename,
-          data: fs.createReadStream(path.resolve(a.path)),
-          cid: a.cid,
-        })) ?? [];
-
       const data: any = {
         from: this.from,
         to,
@@ -46,12 +34,13 @@ export class MailerService {
         html,
       };
 
-      if (formattedAttachments.length > 0) {
-        data.attachment = formattedAttachments.map((a) => a.data);
+      if (attachments && attachments.length > 0) {
+        data.attachment = attachments.map((a) =>
+          fs.createReadStream(path.resolve(a.path)),
+        );
       }
 
-      const result = await this.mg.messages.create(this.domain, data);
-
+      const result = await this.mg.messages().send(data);
       this.logger.log(`✅ Email enviado com sucesso: ${result.id}`);
       return result;
     } catch (error) {
