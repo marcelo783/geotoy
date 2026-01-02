65e495690d9c0b067b3c9a2eb77cfd1039728737
marcelo783 <marcelo.oliveira7931@gmail.com>
2026-01-02 16:32:41 -0300

    username

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module'
   type: 'postgres',
   host: process.env.DB_HOST,   
   port: Number(process.env.DB_PORT), 
-  username: process.env.DB_USERNAME, 
+  username: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,     
   autoLoadEntities: true,
